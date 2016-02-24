4c89178d7926fc56df2dfce2d078c7a759a1f134
MaMuyu <mamuyu@MaMuyus-MBP.attlocal.net>
2016-02-24 01:16:46 -0800

    done?

--- PA2.java
+++ PA2.java
@@ -1,79 +1,94 @@
-/**
- * This Java program exemplifies the basic usage of JDBC.
- * Requirements:
- *   (1) JDK 1.6+.
- *   (2) SQLite3.
- *   (3) SQLite3 JDBC jar (https://bitbucket.org/xerial/sqlite-
-jdbc/downloads/sqlite-jdbc-3.8.7.jar).
- */
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-public class PA2 {
-public static void main(String[] args) {
-    Connection conn = null;  // Database connection.
-    try {
-      // Load the JDBC class.
-      Class.forName("org.sqlite.JDBC");
-      // Get the connection to the database.
-      // - "jdbc" : JDBC connection name prefix.
-      // - "sqlite" : The concrete database implementation
-      // (e.g., sqlserver, postgresql).
-      // - "pa2.db" : The name of the database. In this project,
-      // we use a local database named "pa2.db".  This can also
- // be a remote database name.
- conn = DriverManager.getConnection("jdbc:sqlite:pa2.db");
- System.out.println("Opened database successfully.");
- // Use case #1: Create and populate a table.
- // Get a Statement object.
- Statement stmt = conn.createStatement();
- stmt.executeUpdate("DROP TABLE IF EXISTS Student;");
-// Student table is being created just as an example. You
-// do not need Student table in PA2
-stmt.executeUpdate(
-     "CREATE TABLE Student(FirstName, LastName);");
- stmt.executeUpdate(
-"INSERT INTO Student VALUES('F1','L1'),('F2','L2');");
- // Use case #2: Query the Student table with Statement.
- // Returned query results are stored in a ResultSet
- // object.
- ResultSet rset = stmt.executeQuery("SELECT * from Student;");
- // Print the FirstName and LastName columns.
- System.out.println ("\nStatement result:");
- // This shows how to traverse the ResultSet object.
- while (rset.next()) {
-   // Get the attribute value.
-   System.out.print(rset.getString("FirstName"));
-   System.out.print("---");
-   System.out.println(rset.getString("LastName"));
-}
- // Use case #3: Query the Student table with
- // PreparedStatement (having wildcards).
- PreparedStatement pstmt = conn.prepareStatement(
-     "SELECT * FROM Student WHERE FirstName = ?;");
- // Assign actual value to the wildcard.
- pstmt.setString (1, "F1");
- rset = pstmt.executeQuery ();
- System.out.println ("\nPrepared statement result:");
- while (rset.next()) {
-   System.out.print(rset.getString("FirstName"));
 
-System.out.print("---");
-        System.out.println(rset.getString("LastName"));
-      }
-      // Close the ResultSet and Statement objects.
-      rset.close();
-      stmt.close();
-    } catch (Exception e) {
-      throw new RuntimeException("There was a runtime problem!", e);
-    } finally {
-      try {
-        if (conn != null) conn.close();
-      } catch (SQLException e) {
-        throw new RuntimeException(
-          "Cannot close the connection!", e);
-} }
-} }
+public class PA2{
+	public static void main(String[] args){
+		Connection conn = null;
+		try{
+			Class.forName("org.sqlite.JDBC");
+			conn = DriverManager.getConnection("jdbc:sqlite:pa2.db");
+ 			System.out.println("Opened database successfully.");
+ 			
+ 			Statement statement = conn.createStatement();
+ 			ResultSet set = null;
+ 			
+ 			statement.executeUpdate("DROP TABLE IF EXISTS Connected;");
+ 			statement.executeUpdate(
+ 			"CREATE TABLE Connected(Airline char(32), Origin char(32), Destination char(32), Stops INT);");
+			//statement.executeUpdate("INSERT INTO Connected VALUES('F1','L1'),('F2','L2');");
+			
+			//create a temporary table for usage
+			statement.executeUpdate("DROP TABLE IF EXISTS Temp;");
+			statement.executeUpdate(
+			"CREATE TABLE Temp(Airline char(32), Origin char(32), Destination char(32), Stops INT);");
+			//create table result, might contain duplicates
+			statement.executeUpdate("DROP TABLE IF EXISTS Result;");
+			statement.executeUpdate(
+			"CREATE TABLE Result(Airline char(32), Origin char(32), Destination char(32), Stops INT);");
+			
+			//initialize result and temp to have all the direct flight with 0 stop
+			statement.executeUpdate("INSERT INTO Result SELECT *," + 0 + " FROM Flight;");
+			
+			
+			//update the table 1 stop at a time
+			int stop = 1;
+			while(true){
+				statement.executeUpdate("INSERT INTO Temp SELECT * FROM Result;");
+				set = statement.executeQuery("SELECT COUNT(*) FROM Result;");
+				int setSize = set.getInt(1);
+				statement.executeUpdate(
+				"INSERT INTO Result "+
+				"SELECT t.Airline, f.Origin, t.Destination, "+stop+
+				" FROM Temp t, Flight f "+
+				"WHERE f.Airline=t.Airline AND "+
+				"(f.Destination=t.Origin AND f.Origin<>t.Destination) "+
+				"AND t.Stops="+(stop-1)+
+				" AND NOT EXISTS(SELECT * FROM Result r "+
+								"WHERE r.Airline=t.Airline AND r.Origin=f.Origin AND r.Destination=t.Destination);");
+				statement.executeUpdate(
+				"INSERT INTO Result "+
+				"SELECT t.Airline, t.Origin, f.Destination, "+stop+
+				" FROM Temp t, Flight f "+
+				"WHERE f.Airline=t.Airline AND "+
+				"(f.Destination<>t.Origin AND f.Origin=t.Destination) "+
+				"AND t.Stops="+(stop-1)+
+				" AND NOT EXISTS(SELECT * FROM Result r "+
+								"WHERE r.Airline=t.Airline AND r.Origin=t.Origin AND r.Destination=f.Destination);");
+				set = statement.executeQuery("SELECT COUNT(*) FROM Result;");
+				int setSize2 = set.getInt(1);
+				if(setSize==setSize2)break;
+				stop++;
+				statement.executeUpdate("DELETE FROM Temp;");
+			}
+			statement.executeUpdate("DROP TABLE IF EXISTS Temp;");
+			statement.executeUpdate("INSERT INTO Connected "+
+									"SELECT Airline, Origin, Destination, MIN(Stops) FROM Result "+
+									"GROUP BY Airline, Origin, Destination ;");
+			statement.executeUpdate("DROP TABLE IF EXISTS Result;");
+			statement.close();
+			set.close();
+		}
+		catch (Exception e)
+		{
+		  throw new RuntimeException("There was a runtime problem!", e);
+		}
+		finally
+		{
+		  try
+		  {
+			if(conn != null)
+			{
+			  conn.close();
+			}
+		  }
+		  catch (SQLException e)
+		  {
+			throw new RuntimeException("Cannot close the connection!", e);
+		  }
+		}
+	}
+}
